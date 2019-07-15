8166e6e97bc75b4f23f7fa514010930a7d6b5798
Boon <doctorpot1@hotmail.com>
2019-07-15 17:27:35 +0800

    Added a Simple Vue component that show you the time and refresh itself.

---
+++ timebar/timebar.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <span class="font-weight-light">{{prefix}}</span>
+        <span class="font-weight-bold">{{time}}</span>
+        <span class="font-weight-light">{{suffix}}</span>
+    </div>
+</template>
+
+<script>
+    import moment from "moment";
+
+    export default {
+        name: "timebar",
+        data: () => ({
+            timeNow: moment()
+        }),
+        mounted() {
+            setInterval(() => {
+                this.timeNow = moment();
+            }, this.counter);
+        },
+        computed: {
+            time: function() {
+                return this.timeNow.format(this.format);
+            }
+        },
+        props: {
+            prefix: {
+                    type: String,
+                    default() { return ""; }
+            },
+            suffix: {
+                type: String,
+                default() { return ""; }
+            },
+            format: {
+                type: String,
+                default() { return "hh:mm:ss A"; }
+            },
+            counter: {
+                type: Number,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
